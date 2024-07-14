9d8b03e8944643ae2bc57499278910fb81d37d65
dkavla <kowal.dawid487@gmail.com>
2024-07-14 00:00:23 -0400

    Create UI for app

--- RockPaperScissorsQuestion/RockPaperScissorsQuestion/ContentView.swift
+++ RockPaperScissorsQuestion/RockPaperScissorsQuestion/ContentView.swift
@@ -7,22 +7,92 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var choices = ["Rock", "Paper", "Scissors"]
-    @State private var currentChoice = "Rock"
-    @State private var win = true
+struct EmojiQuestion: View {
+    var text: String
     
-    @State private var playerChoice = ""
+    var body: some View {
+        Text(text)
+            .font(.system(size: 80))
+            .padding()
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
+
+
+struct EmojiChoices: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.system(size: 60))
+            .padding()
+            .background(.orange)
+            .clipShape(.capsule)
+    }
+}
+
+struct ContentView: View {
+    @State private var choices = ["🪨", "📄", "✂️"]
+    @State private var currentChoice = "🪨"
+    @State private var win = Bool.random()
+    @State private var displayOutcome = ""
+    
+    @State private var score = 0
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Spacer()
+                HStack {
+                    EmojiQuestion(text: currentChoice)
+                }
+                .padding()
+                HStack {
+                    Text("Which option to \(win ? "Win" : "Lose")?")
+                        .font(.system(size: 25))
+                }
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        evaluateResult(question: currentChoice, user: choices[1], outcome: win)
+                        
+                    }, label: {
+                        EmojiChoices(text: choices[1])
+                    })
+                    Spacer()
+                    Button(action: {
+                        evaluateResult(question: currentChoice, user: choices[2], outcome: win)
+                    }, label: {
+                        EmojiChoices(text: choices[2])
+                    })
+                    Spacer()
+                }
+                HStack {
+                    Text("Score: \(score)")
+                        .padding()
+                        .font(.title)
+                }
+                Spacer()
+            }
         }
-        .padding()
+        .ignoresSafeArea()
     }
+    
+    func evaluateResult(question: String, user: String, outcome: Bool) {
+        // if outcome is to win
+        if outcome {
+            if (question == "🪨" && user == "📄") || (question == "📄" && user == "✂️") || (question == "✂️" && user == "🪨"){
+                score += 1
+            } else {
+                if score > 0 {
+                    score -= 1
+                }
+            }
+        }
+    }
+    
 }
 
 #Preview {
